fd6bc4ea540cdda2c8eb96e7d7bab2b8b370b35e
BelisAliosmanova <benidebelis@gmail.com>
2024-07-15 14:39:10 +0300

    Initial commit

---
+++ src/main/java/com/machines/machines_api/MachinesApiApplication.java
@@ -0,0 +1,13 @@
+package com.machines.machines_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachinesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachinesApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/machines/machines_api/TestController.java
@@ -0,0 +1,17 @@
+package com.machines.machines_api;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+@AllArgsConstructor
+public class TestController {
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello, World!");
+    }
+}

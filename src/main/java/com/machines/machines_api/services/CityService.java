9449acd2635b8d2f2bc1972f6e1d4c75cff80b43
BelisAliosmanova <benidebelis@gmail.com>
2024-07-27 09:29:16 +0300

    fix some things for cities:
     - add undo delete
     - create a methods geById for the Admin
     - fix update methods

--- src/main/java/com/machines/machines_api/services/CityService.java
+++ src/main/java/com/machines/machines_api/services/CityService.java
@@ -2,6 +2,7 @@ package com.machines.machines_api.services;
 
 import com.machines.machines_api.models.dto.request.CityRequestDTO;
 import com.machines.machines_api.models.dto.response.CityResponseDTO;
+import com.machines.machines_api.models.dto.response.admin.CityAdminResponseDTO;
 import com.machines.machines_api.models.entity.City;
 
 import java.util.List;
@@ -10,7 +11,11 @@ import java.util.UUID;
 public interface CityService {
     List<CityResponseDTO> getAll();
 
+    List<CityAdminResponseDTO> getAllAdmin();
+
     CityResponseDTO getById(UUID id);
+
+    CityAdminResponseDTO getCategoryByIdAdmin(UUID id);
 
     CityResponseDTO create(CityRequestDTO cityRequestDTO);
 
@@ -19,4 +24,6 @@ public interface CityService {
     void delete(UUID id);
 
     City getEntityById(UUID id);
+
+    City getEntityByIdAdmin(UUID id);
 }

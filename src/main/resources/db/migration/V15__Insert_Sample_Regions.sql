6514c08c732ea15ee03e48fb3daf5055ae173978
BelisAliosmanova <benidebelis@gmail.com>
2024-09-21 12:34:38 +0300

    Insert countries, cities, categories and subcategories via migrations in the database

--- src/main/resources/db/migration/V15__Insert_Sample_Regions.sql
+++ src/main/resources/db/migration/V15__Insert_Sample_Regions.sql
@@ -1,12 +1,4 @@
 -- Insert sample regions
 INSERT INTO regions (id, created_at, updated_at, deleted_at, name, country_id)
-VALUES (uuid_generate_v4(), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL, 'California',
-        (SELECT id FROM countries WHERE name = 'United States')),
-       (uuid_generate_v4(), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL, 'Ontario',
-        (SELECT id FROM countries WHERE name = 'Canada')),
-       (uuid_generate_v4(), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL, 'Jalisco',
-        (SELECT id FROM countries WHERE name = 'Mexico')),
-       (uuid_generate_v4(), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL, 'São Paulo',
-        (SELECT id FROM countries WHERE name = 'Brazil')),
-       (uuid_generate_v4(), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL, 'Buenos Aires',
-        (SELECT id FROM countries WHERE name = 'Argentina'));
+VALUES (uuid_generate_v4(), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL, 'България',
+        (SELECT id FROM countries WHERE name = 'България'));
